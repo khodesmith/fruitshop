0af90583887ec787d2bfdb991ae69520b3bc2091
Ojeyinka Abdul Azeez <azeezojeyinka@yahoo.com>
2023-01-02 03:17:29 +0100

    Second upload, added Total Price and Pay now

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -10,46 +10,99 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("My Cart"),
-        ),
-        body: Consumer<CardModel>(
-          builder: (context, value, child) {
-            return Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: value.cartItems.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(8)),
-                          child: ListTile(
-                            leading: Image.asset(
-                              value.cartItems[index]["image"],
-                              height: 36,
-                            ),
-                            title: Text(value.cartItems[index]["name"]),
-                            subtitle:
-                                Text("\$${value.cartItems[index]["price"]}"),
-                            trailing: IconButton(
-                              onPressed: () =>
-                                  Provider.of<CardModel>(context, listen: false)
-                                      .removeItemFromCart(index),
-                              icon: const Icon(Icons.cancel),
-                            ),
+      appBar: AppBar(
+        title: const Text("My Cart"),
+      ),
+      body: Consumer<CardModel>(
+        builder: (context, value, child) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: value.cartItems.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(8)),
+                        child: ListTile(
+                          leading: Image.asset(
+                            value.cartItems[index]["image"],
+                            height: 36,
+                          ),
+                          title: Text(value.cartItems[index]["name"]),
+                          subtitle:
+                              Text("\$${value.cartItems[index]["price"]}"),
+                          trailing: IconButton(
+                            onPressed: () =>
+                                Provider.of<CardModel>(context, listen: false)
+                                    .removeItemFromCart(index),
+                            icon: const Icon(Icons.cancel),
                           ),
                         ),
-                      );
-                    },
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green[400],
+                      borderRadius: BorderRadius.circular(8)),
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Total Price",
+                            style: TextStyle(color: Colors.green[100]),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "\$${value.calculateTotal()}",
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.green.shade100),
+                            borderRadius: BorderRadius.circular(12)),
+                        padding: const EdgeInsets.all(12),
+                        child: Row(
+                          children: const [
+                            Text(
+                              "Pay Now",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios_rounded,
+                              size: 16,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            );
-          },
-        ));
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
